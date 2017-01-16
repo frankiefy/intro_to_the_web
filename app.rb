503be63002d49a0a6e034a1f031bc4f3fa30252f
frankie <franklin.shaw@gmail.com>
2017-01-16 16:33:27 +0000

    end of step12

--- app.rb
+++ app.rb
@@ -10,7 +10,13 @@ get '/secret' do
   "bang This route is /secret (added onto the URL)"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
